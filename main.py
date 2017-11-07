992b9ca6223a778adeaa1b7359e858979099fafa
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-11-07 19:32:35 +0530

    graph structure added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print "hello world"
+Graph = {
+			'iec2014071' :  {
+								'authorName': 'Saloni Mittal',
+								'associations': [ 'Abhishek Deora', 'Pooja Shukla'],
+								'tags': [ 'machine learning'] 
+							},
+			'iit2014141' :  {
+								'authorName': 'Abhishek Deora',
+								'associations': [ 'Saloni Mittal', 'Pooja Shukla'],
+								'tags': [ 'machine learning'] 
+							}
+		}
+print Graph
