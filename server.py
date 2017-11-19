ea52020e13360abcfa2d155c8db1c32593961621
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-11-20 03:30:18 +0530

    web interface added

---
+++ server.py
@@ -0,0 +1,94 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+import json, urlparse
+import string,cgi,time
+from os import curdir, sep
+from my_inverted_index import display_results
+
+class GetHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        parsed_path = urlparse.urlparse(self.path)
+        
+        if self.path=="/":
+            self.path="/views/index.html"
+
+        try:
+            sendReply = False
+            if self.path.endswith(".html"):
+                mimetype='text/html'
+                sendReply = True
+            if self.path.endswith(".jpg"):
+                mimetype='image/jpg'
+                sendReply = True
+            if self.path.endswith(".png"):
+                mimetype='image/png'
+                sendReply = True
+            if self.path.endswith(".gif"):
+                mimetype='image/gif'
+                sendReply = True
+            if self.path.endswith(".svg"):
+                mimetype='image/svg+xml'
+                sendReply = True
+            if self.path.endswith(".css"):
+                mimetype='text/css'
+                sendReply = True
+            if self.path.endswith(".js"):
+                mimetype='application/javascript'
+                sendReply = True
+            if self.path.endswith(".ttf"):
+                mimetype='application/x-font-ttf'
+                sendReply = True
+            if self.path.endswith(".otf"):
+                mimetype='application/x-font-opentype'
+                sendReply = True
+            if self.path.endswith(".woff"):
+                mimetype='application/font-woff'
+                sendReply = True
+
+            if sendReply == True:
+                #Open the static file requested and send it
+                f = open(curdir + sep + self.path, 'rb')
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+                print(curdir + sep + self.path)
+
+            elif (len(self.path.split('=')) == 2)  :
+                mimetype = 'text/html'
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                print "hello world"
+                result = display_results(self.path.split('=')[1])
+                c = "";
+                for a in result:
+                    for b in a:
+                        c = c +","+b
+                self.wfile.write(c)
+                print(curdir + sep + self.path)
+            return
+
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+
+        if self.path=="/run":
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                         'CONTENT_TYPE':self.headers['Content-Type'],
+            })
+
+
+                        #self.send_response(200)
+                        #self.end_headers()
+            return
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('localhost', 8080), GetHandler)
+    print 'Starting server at http://localhost:8080'
+    server.serve_forever()
---
+++ views/main.js
@@ -0,0 +1,39 @@
+function keyDown(e){
+	if(e.keyCode == 13){
+		search();
+	}
+}
+
+function search()
+{
+	console.log("searching...");
+	var res = document.getElementById("results");
+	res.innerHTML = "Fetchin Results";
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       console.log(xhttp.responseText);
+	       var results = xhttp.responseText;
+	       results = results.substr(1);
+	       results = results.split(',');
+	       var data = ''
+	       let count = (results.length)/6;
+	       for(let i =0; i<count; i++)
+	       {
+	       		data = data + '<div class="eachResult">'+
+									'<div class="authorInfo">'+ results[i*6]+'[ '+results[i*6+1]+' - '+results[i*6+2]+' ]</div>'+
+									'<div class="authorPubInfo">'+
+										'<div>Total Piblication: '+results[i*6+3]+'</div>'+
+										'<div>Citation count: '+results[i*6+4]+'</div>'+
+										'<div>Average Citation: '+results[i*6+5]+'</div>'+
+									'</div>'+
+								'</div>';
+	       }
+	       res.innerHTML = data;
+	    }
+	};
+	var query = document.getElementById("searchBox").value;
+	console.log(query);
+	xhttp.open("GET", "?query=" + query, true);
+	xhttp.send();
+}
